993e2febd793f6f2d7268caee1d7fb0e175e7303
sayedalfeki <selfeki207@gmail.com>
2024-08-26 12:13:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,23 @@ def welcomeApp():
 @app.get('/isholiday')
 def isHoliday(year:int,month:int,day:int):
     selectedDate=datetime.datetime(year=year,month=month,day=day)
-    dates=[datetime.datetime(year=2024,month=7,day=11),datetime.datetime(year=2024,month=9,day=19)]
+    dates=[datetime.datetime(year=2024,month=7,day=11),
+           datetime.datetime(year=2024,month=7,day=25),
+           datetime.datetime(year=2024,month=9,day=19),
+           datetime.datetime(year=2024,month=10,day=6),
+           datetime.datetime(year=2025,month=1,day=9),
+           datetime.datetime(year=2025,month=3,day=31),
+           datetime.datetime(year=2025,month=1,day=30),
+           datetime.datetime(year=2025,month=4,day=1),
+           datetime.datetime(year=2025,month=4,day=2),
+           datetime.datetime(year=2025,month=4,day=21),
+           datetime.datetime(year=2025,month=7,day=24),
+           datetime.datetime(year=2025,month=5,day=1),
+           datetime.datetime(year=2025,month=6,day=7),
+           datetime.datetime(year=2025,month=6,day=8),
+           datetime.datetime(year=2025,month=6,day=9),
+           datetime.datetime(year=2025,month=6,day=26),
+           ]
     return {'isHoliday':dates.__contains__(selectedDate)}    
 # app.include_router(users.router)
 # app.include_router(items.router)
